3edbb5922088a2869068db3bd587192c58459345
Shambhavi Sharma <shambhavisharma8235@gmail.com>
2026-07-30 11:47:19 +0530

    Update StackB and add stock span solution

--- Stack/StackB.java
+++ Stack/StackB.java
@@ -1,6 +1,50 @@
 import java.util.*;
+public class StackB{
+  //push at the bottom
+public static void pushAtBottom(Stack<Integer> s,int data){
+  
+  if(s.isEmpty()){
+    s.push(data);
+    return;
+  }
+  int top=s.pop();
+  pushAtBottom(s,data);
+  s.push(top);
+}
+public static void reverseStack(Stack<Integer>s){
+  if(s.isEmpty()){
+    return;
+  }
+  int top=s.pop();
+  reverseStack(s);
+  pushAtBottom(s,top);
 
-public class StackB {
+}
+public static void printStack(Stack<Integer> s){
+  while(!s.isEmpty()){
+    System.out.println(s.pop());
+  }
+}
+//reverse the string in astack
+// public static String  reverseString(String str){
+//   Stack<Character> s=new Stack<>();
+//   int idx=0;
+//   //push all  character into stack
+//   while(idx<str.length()){
+//     s.push(str.charAt(idx));
+//     idx++;
+//   }
+//   //create empty result
+//   StringBuilder result=new StringBuilder("");
+//   //pop characters and append
+//   while(!s.isEmpty()){
+//     char curr=s.pop();
+//     result.append(curr);
+//   }
+  
+     
+//    return result.toString();
+// }
 
     // static class Node {
     //     int data;
@@ -79,17 +123,34 @@ public class StackB {
             
           // }
       public static void main(String[] args) {
-
-        // Stack s = new Stack();
-
- Stack<Integer> s=new Stack<>();
+        Stack<Integer> s=new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
+        s.push(4);
+        printStack(s);
+        reverseStack(s);
+        printStack(s);
 
-        while (!s.isEmpty()) {
-            System.out.println(s.peek());
-            s.pop();
-        }
-    }
+
+        // Stack s = new Stack();
+
+    // Stack<Integer> s=new Stack<>();
+    //    s.push(1);
+    //     s.push(2);
+    //     s.push(3);
+    //     pushAtBottom(s,4);
+
+//         while (!s.isEmpty()) {
+//             System.out.println(s.peek());
+//             s.pop();
+//         }
+      //  while(!s.isEmpty()){
+      //   System.out.println(s.pop());
+      //  }
+    //   String str="HELLOWORLD";
+    //   String result=reverseString(str);
+    //   System.out.println(result);
+    // }
+  }
 }
