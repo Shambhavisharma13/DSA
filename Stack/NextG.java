1f0308f35f3974ca71ebbaea49e43cb766922772
Shambhavi Sharma <shambhavisharma8235@gmail.com>
2026-07-31 10:33:38 +0530

    Add Next Greater Element solution

---
+++ Stack/NextG.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class NextG {
+    public static void main(String args[]){
+        int arr[]={6,8,0,1,3};
+        Stack<Integer> s=new Stack<>();
+        int nxtGreater[]=new int[arr.length];
+        for(int i=arr.length-1;i>=0;i--){
+            //1 while
+   while(!s.isEmpty() &&arr[s.peek()]<=arr[i]){
+    s.pop();
+   }
+
+            //2 if-else
+            if(s.isEmpty()){
+              nxtGreater[i]=-1;  
+            }else {
+                nxtGreater[i]=arr[s.peek()];
+            }
+
+
+            //3 push in s
+            s.push(i);
+        }
+        for(int i=0;i<nxtGreater.length;i++){
+            System.out.print(nxtGreater[i]+" ");
+        }
+        System.out.println();
+        //next greater right
+        //next greater left
+        //next smaller right
+        //next smaller left
+    }
+}
+//Time complexity of this coe is o(3n) but we ignore the constant soo o(n) is the time complexity
