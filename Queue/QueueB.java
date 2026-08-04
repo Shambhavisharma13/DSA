566dde8669fc3d1a476da5147a888c660daea5c0
Shambhavi Sharma <shambhavisharma8235@gmail.com>
2026-08-04 12:38:44 +0530

    Add circular queue implementation using array with enqueue, dequeue, and peek operations

--- Queue/QueueB.java
+++ Queue/QueueB.java
@@ -5,22 +5,31 @@ public class QueueB {
      static  int arr[];
      static int size;
      static int rear;
+     static int front;
 
        Queue(int n){
         arr=new int[n];
         size=n;
         rear=-1;
+        front=-1;
        }
        public static boolean isEmpty(){
-        return rear==-1;
+        return rear==-1 && front== -1;
+       }
+       public static boolean isFull(){
+        return (rear+1)%size== front;
        }
        //add
        public static void add(int data){
-        if(rear==size-1){
-            System.out.println("Queue is full");
+     //   if(rear==size-1){
+     if(isFull()){
+          System.out.println("Queue is full");
             return;
         }
-        rear=rear+1;
+        if(front==-1){
+         front =0;   
+        }
+        rear=(rear+1)%size;
         arr[rear]=data;
        }
        //remove
@@ -29,12 +38,22 @@ public class QueueB {
            System.out.println("empty queue");
            return -1; 
         }
-        int front=arr[0];
- for(int i=0;i<rear;i++){
-    arr[i]=arr[i+1];
- }
- rear=rear-1;
-        return front;
+      
+        
+//  for(int i=0;i<rear;i++){
+//     arr[i]=arr[i+1];
+//  }
+int result=arr[front];
+front=(front+1)%size;
+//last el delete'
+if(rear==front){
+    rear=front=-1;
+}else{
+    front=(front+1)%size;
+}
+//  rear=rear-1;
+        return result;
+        // return front;
        }
        //peek
        public static int peek(){
@@ -42,7 +61,8 @@ public class QueueB {
             System.out.println("empty queue");
             return -1;
         }
-        return arr[0];
+        // return arr[0];
+        return arr[front];
        }
     }
     public static void main(String[] args) {
@@ -50,7 +70,11 @@ public class QueueB {
         q.add(1);
         q.add(2);
         q.add(3);
+        
+        System.out.println(q.remove());
         q.add(4);
+        System.out.println(q.remove());
+          q.add(5);
         while(!q.isEmpty()){
             System.out.println(q.peek());
             q.remove();
