5f68f7737f29edca03aeccbcb2fdf15092a47d2b
Shambhavi Sharma <shambhavisharma8235@gmail.com>
2026-08-17 19:07:46 +0530

    print in range

--- Binary search tree/BST.java
+++ Binary search tree/BST.java
@@ -1,66 +1,166 @@
-
 public class BST {
-    static class Node{
+
+    static class Node {
         int data;
         Node left;
         Node right;
 
-        Node(int data){
-            this.data=data;
+        Node(int data) {
+            this.data = data;
         }
     }
-public static Node insert(Node root,int val){
- if(root == null){
-    root=new Node(val);
-    return root;
- }
 
- if(root.data>val){
-    //left subtree
-   root.left= insert(root.left,val);
- }else{
-    //right subtre
-    root.right= insert(root.right,val);
- }
- return root;
-}
-public static void inorder(Node root){
-    if(root==null){
-        return;
-    }
-    inorder(root.left);
-    System.out.print(root.data +" ");
-    inorder(root.right);
-}
-public static boolean search(Node root,int key){
-    if(root==null){
-        return false;
-    }
-    if(root.data==key){
-        return true;
-    }
-    if(root.data>key){
-      return  search(root.left,key);
-    }else{
-        return search(root.right,key);
-    }
-}
+    // INSERT
+    public static Node insert(Node root, int val) {
 
-
-    public static void main(String args[]){
-        int values[]={5,1,3,4,2,7};
-        Node root=null;
-
-        for(int i=0;i<values.length;i++){
-            root=insert(root,values[i]);
+        if (root == null) {
+            root = new Node(val);
+            return root;
         }
+
+        if (root.data > val) {
+            root.left = insert(root.left, val);
+        }
+        else {
+            root.right = insert(root.right, val);
+        }
+
+        return root;
+    }
+
+    // INORDER
+    public static void inorder(Node root) {
+
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    // SEARCH
+    public static boolean search(Node root, int key) {
+
+        if (root == null) {
+            return false;
+        }
+
+        if (root.data == key) {
+            return true;
+        }
+
+        if (root.data > key) {
+            return search(root.left, key);
+        }
+        else {
+            return search(root.right, key);
+        }
+    }
+
+    // DELETE
+    public static Node delete(Node root, int val) {
+
+        if (root == null) {
+            return null;
+        }
+
+        if (root.data < val) {
+            root.right = delete(root.right, val);
+        }
+        else if (root.data > val) {
+            root.left = delete(root.left, val);
+        }
+        else {
+
+            // Case 1: No child
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+
+            // Case 2: Only right child
+            if (root.left == null) {
+                return root.right;
+            }
+
+            // Case 2: Only left child
+            else if (root.right == null) {
+                return root.left;
+            }
+
+            // Case 3: Two children
+            Node IS = findInorderSuccessor(root.right);
+
+            root.data = IS.data;
+
+            root.right = delete(root.right, IS.data);
+        }
+
+        return root;
+    }
+
+    // INORDER SUCCESSOR
+    public static Node findInorderSuccessor(Node root) {
+
+        while (root.left != null) {
+            root = root.left;
+        }
+
+        return root;
+    }
+
+    // PRINT VALUES IN RANGE
+    public static void printInRange(Node root, int k1, int k2) {
+
+        if (root == null) {
+            return;
+        }
+
+        if (root.data >= k1 && root.data <= k2) {
+
+            printInRange(root.left, k1, k2);
+
+            System.out.print(root.data + " ");
+
+            printInRange(root.right, k1, k2);
+        }
+
+        else if (root.data < k1) {
+
+            printInRange(root.right, k1, k2);
+        }
+
+        else {
+
+            printInRange(root.left, k1, k2);
+        }
+    }
+
+    public static void main(String args[]) {
+
+        int values[] = {8, 5, 3, 1, 6, 10, 11, 14};
+
+        Node root = null;
+
+        // Create BST
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+
+        // Inorder
         inorder(root);
         System.out.println();
-        if(search(root,8)){
-            System.out.println("found");
-        }else{
-            System.out.println("Not found");
-        }
 
+        // Delete
+        root = delete(root, 1);
+
+        System.out.println("After deleting 1:");
+        inorder(root);
+        System.out.println();
+
+        // Print range
+        System.out.println("Values between 5 and 12:");
+        printInRange(root, 5, 12);
     }
 }
