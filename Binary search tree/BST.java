1becf63b6e2ac79bb63cdd67e1bc4cc6fce058e2
Shambhavi Sharma <shambhavisharma8235@gmail.com>
2026-08-19 10:38:52 +0530

    path of root to leaf

--- Binary search tree/BST.java
+++ Binary search tree/BST.java
@@ -1,3 +1,5 @@
+
+import java.util.*;
 public class BST {
 
     static class Node {
@@ -136,6 +138,24 @@ public class BST {
             printInRange(root.left, k1, k2);
         }
     }
+    public static void printPath(ArrayList<Integer> path){
+        for(int i=0;i<path.size();i++){
+            System.out.print(path.get(i)+"->");
+        }
+        System.out.println("N");
+    }
+    public static void printRoot2Leaf(Node root,ArrayList<Integer> path){
+     if(root==null){
+        return;
+     }
+  path.add(root.data);
+     if(root.left==null && root.right==null){
+            printPath(path);
+        }
+  printRoot2Leaf(root.left,path);
+  printRoot2Leaf(root.right,path);
+  path.remove(path.size()-1);
+    }
 
     public static void main(String args[]) {
 
@@ -162,5 +182,7 @@ public class BST {
         // Print range
         System.out.println("Values between 5 and 12:");
         printInRange(root, 5, 12);
+
+        printRoot2Leaf(root,new ArrayList<>());
     }
 }
