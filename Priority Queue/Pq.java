4731ee839ee503dba8280af9b40ef92c073ccafc
Shambhavi Sharma <shambhavisharma8235@gmail.com>
2026-08-22 08:34:49 +0530

    Implement PriorityQueue with custom Student comparator

--- Priority Queue/Pq.java
+++ Priority Queue/Pq.java
@@ -1,15 +1,33 @@
 import java.util.*;
+
 public class Pq {
+  static class Student implements Comparable<Student>{//overriding
+    String name;
+    int rank;
+
+    public Student(String name,int rank){
+        this.name=name;
+        this.rank=rank;
+    }
+    @Override
+    public int compareTo(Student s2){
+        return this.rank-s2.rank;
+    }
+
+  }
+
     public static void main(String args[]){
-     PriorityQueue<Integer>pq=new PriorityQueue<>(Comparator.reverseOrder());
-      pq.add(3);//o(logn)
-      pq.add(4);
-      pq.add(1);
-      pq.add(7);
+     PriorityQueue<Student>pq=new PriorityQueue<>(Comparator.reverseOrder());
+      pq.add(new Student("A",4));//o(logn)
+      pq.add(new Student("B",5));//o(logn)
+      pq.add(new Student("C",2));//o(logn)
+      pq.add(new Student("D",30));//o(logn)
+      pq.add(new Student("E",45));//o(logn)
+     
 
 
     while(!pq.isEmpty()){
-        System.out.println(pq.peek());//o(1)
+        System.out.println(pq.peek().name+"=>"+pq.peek().rank);//o(1)
            pq.remove();//O(logn)
     }
     }
