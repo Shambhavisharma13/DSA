cfbba9e1fb33b8a23ee957ab17b25d27a3073b4c
Shambhavi Sharma <shambhavisharma8235@gmail.com>
2026-09-12 09:51:05 +0530

    addition of 0 nodes in the graph

--- GRAPH/Path.java
+++ GRAPH/Path.java
@@ -14,6 +14,8 @@ public class Path {
         for(int i=0;i<graph.length;i++){
             graph[i]=new ArrayList<>();
         }
+
+        graph[0].add(new Edge(0,3));
         graph[2].add(new Edge(2,3));
 
         graph[3].add(new Edge(3,1));
@@ -24,6 +26,7 @@ public class Path {
         graph[5].add(new Edge(5,0));
         graph[5].add(new Edge(5,2));
     }
+    //exponential time complexity
     public static void printAllPath(ArrayList<Edge> graph[],int src,int dest,String path){
         if(src==dest){
          System.out.println(path+dest);
