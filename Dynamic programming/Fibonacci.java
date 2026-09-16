c35b2cf6393d90fce87b59182417ede6bffc8316
Shambhavi Sharma <shambhavisharma8235@gmail.com>
2026-09-16 14:08:17 +0530

    Implement Fibonacci using DP memoization

--- Dynamic programming/Fibonacci.java
+++ Dynamic programming/Fibonacci.java
@@ -1,13 +1,18 @@
 
 public class Fibonacci{
-    public static int fib(int n){
+    public static int fib(int n,int f[]){
         if(n==0|| n==1){ //0 1 2 3          
               return n;
         }
-         return fib(n-1)+fib(n-2);
+        if(f[n]!=0){
+          return f[n];  
+        }
+          f[n]=fib(n-1,f)+fib(n-2,f);
+          return f[n];
     }
     public static void main(String[] args) {
         int n=5;
-        System.out.println(fib(n));
+        int f[]=new int[n+1];// 0,0,0,0
+        System.out.println(fib(n,f));
     }
 }
