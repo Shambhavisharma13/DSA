81b9a094ba9046087059b840bc2006660a6d9549
Shambhavi Sharma <shambhavisharma8235@gmail.com>
2026-09-18 00:25:23 +0530

    implementation of climbing stairs with tabulation method

--- Dynamic programming/Climbing.java
+++ Dynamic programming/Climbing.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Climbing {
-
+//memoization-O(n)| recursion (2^n)
     public static int countWays(int n, int ways[]) {
 
         // Base case
@@ -24,6 +24,21 @@ public class Climbing {
 
         return ways[n];
     }
+    //tabulation
+    public static int countWaysTab(int n){
+        int dp[]=new int[n+1];//0 to n
+        dp[0]=1;
+     for(int i=1;i<=n;i++){
+        if(i==1){
+            dp[i]=dp[i-1];
+
+        }else{
+ dp[i]=dp[i-1]+dp[i-2];
+        }
+       
+     }
+     return dp[n];
+  }
 
     public static void main(String[] args) {
 
@@ -35,5 +50,6 @@ public class Climbing {
         Arrays.fill(ways, -1);
 
         System.out.println(countWays(n, ways));
+        System.out.println(countWaysTab(n));
     }
 }
