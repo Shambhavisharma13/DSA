0f66c64e04a7df127768deaaa297dba10905b329
Shambhavi Sharma <shambhavisharma8235@gmail.com>
2026-09-17 23:22:52 +0530

    Add climbing stairs DP solution

--- Dynamic programming/Climbing.java
+++ Dynamic programming/Climbing.java
@@ -1,15 +1,39 @@
+import java.util.*;
+
 public class Climbing {
-    public static int countWays(int n){
-  if(n==0){
-    return 1;
-  } 
-  if(n<0){
-    return 0;
-  }
-      return  countWays(n-1)+countWays(n-2);
+
+    public static int countWays(int n, int ways[]) {
+
+        // Base case
+        if (n == 0) {
+            return 1;
+        }
+
+        if (n < 0) {
+            return 0;
+        }
+
+        // Already calculated
+        if (ways[n] != -1) {
+            return ways[n];
+        }
+
+        // Memoization
+        ways[n] = countWays(n - 1, ways)
+                + countWays(n - 2, ways);
+
+        return ways[n];
     }
-    
-    public static void main(String args[]){
-        int n=5; //n=3->3& n=4 ->5=>8
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        int ways[] = new int[n + 1];
+
+        // Initially all values are -1
+        Arrays.fill(ways, -1);
+
+        System.out.println(countWays(n, ways));
     }
 }
